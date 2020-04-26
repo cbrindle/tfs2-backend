497c417b0a3cc5ba60c9c8636753c2d9080ea050
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-04-25 23:24:28 -0400

    Updated routes to allow user to sign up for an account.

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(passport.initialize());
 
 passport.use('jwt-user', userJWTStrategy);
 
+app.use(cors({ origin: 'http://localhost:3000', credentials: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
