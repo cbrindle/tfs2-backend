7cf5d7f9042ad77edf7aeb6fd0f7a87f4055f8ec
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-04-26 11:43:56 -0400

    User signin functionality added and tested - good to go.

--- routes/users/controllers/userController.js
+++ routes/users/controllers/userController.js
@@ -32,28 +32,33 @@ module.exports = {
     },
 
     login: async (req, res) => {
+        console.log(req.body);
         try {
             const foundUser = await User.findOne({ email: req.body.email });
             if (foundUser === null) {
+                console.log(`User not found`);
                 throw 'Invalid Email / Password'
             }
 
             const comparePassword = await authHelper.comparePassword(req.body.password, foundUser.password);
             if (comparePassword === 409) {
+                console.log(`Password incorrect`);
                 throw 'Invalid Email / Password'
             }
 
             const jwtToken = await authHelper.createUserToken(foundUser);
-            res.status(200).json({
-                message: 'success',
-                token: jwtToken
-            });
+            console.log(`Success, returning token object...`);
+            const successObj = {
+                token: jwtToken,
+                name: foundUser.name,
+                email: foundUser.email
+            }
+            console.log(successObj);
+            res.json(successObj)
 
         } catch (err) {
             console.log(err);
-            res.status(500).json({
-                message: 'error'
-            })
+            res.status(500).json(err)
         }
     }
 }
